8a25007923ba731c4665c240ac1ed616d96a8c03
Alexis <alexisbou16@gmail.com>
2023-05-15 19:38:51 -0400

    change dog cards, add my card, finish about.html page

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const h2 = document.createElement("h2");
 document.querySelector("body").appendChild(h2);
+
+//Take user back to top//
+const toTop = () => window.scrollTo({top:0, behavior: 'smooth'});
